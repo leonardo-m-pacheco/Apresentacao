9ad2b709ff4910c139cc5c075f30ace696200d2b
leonardo-m-pacheco <92610569+leonardo-m-pacheco@users.noreply.github.com>
2022-03-01 01:41:30 -0300

    .

---
+++ src/ProjetoPlacar/Placar.js
@@ -0,0 +1,33 @@
+let contador1 = 0;
+let contador2 = 0
+
+
+AumentaJ1 = () => {
+
+    document.getElementById('placar1').innerHTML = contador1 += 1;
+}
+DiminuiJ1 = () => {
+
+    document.getElementById('placar1').innerHTML = contador1 -= 1;
+}
+ZeraJ1 = () => {
+
+    document.getElementById('placar1').innerHTML = contador1 = 0;
+}
+
+AumentaJ2 = () => {
+
+    document.getElementById('placar2').innerHTML = contador2 += 1;
+}
+DiminuiJ2 = () => {
+
+    document.getElementById('placar2').innerHTML = contador2 -= 1;
+}
+ZeraJ2 = () => {
+
+    document.getElementById('placar2').innerHTML = contador2 = 0;
+}
+
+
+
+
