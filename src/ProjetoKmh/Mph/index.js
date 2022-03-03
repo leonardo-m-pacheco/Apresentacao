05a818c341d344e38a96bd027b9b269da5a026d8
leonardo-m-pacheco <92610569+leonardo-m-pacheco@users.noreply.github.com>
2022-03-02 23:42:13 -0300

    .

--- src/ProjetoKmh/Mph/index.js
+++ src/ProjetoKmh/Mph/index.js
@@ -5,14 +5,14 @@ let saidamph = document.getElementById('saidamph')
 Convertekm = () =>{
 
 let km = Number(quilometro.value)
-let resultado1 = km * 0.621371
+let resultado1 = (km * 0.621371).toFixed(2)
 
 saida.innerText = `${resultado1} Mph`
 }
 
 Convertemp = () =>{
 let m = Number(milha.value)
-let resultado2 = m * 1.60934421012
+let resultado2 = (m * 1.60934421012).toFixed(2)
 
 saidamph.innerText = `${resultado2} Km/h`
 }
