036a0036ee0493a41f23677180a0abbc86b551e1
leonardo-m-pacheco <92610569+leonardo-m-pacheco@users.noreply.github.com>
2022-03-26 04:09:22 -0300

    .

---
+++ src/teste/arquivo.js
@@ -0,0 +1,9 @@
+var myObject = {"customers":[
+    {"CustomerName":"Leonardo Pacheco","City":"Rio de janeiro","Country":"Brazil"},
+    {"CustomerName":"Around the Horn","City":"London","Country":"UK"},
+    {"CustomerName":"B's Beverages","City":"London","Country":"UK"},
+    {"CustomerName":"Blauer See Delikatessen","City":"Mannheim","Country":"Germany"},
+    {"CustomerName":"Bon app'","City":"Marseille","Country":"France"},
+    {"CustomerName":"Bottom-Dollar Marketse","City":"Tsawassen","Country":"Canada"},
+    {"CustomerName":"Chop-suey Chinese","City":"Bern","Country":"Switzerland"}
+    ]};
