19da422cda97d85a4c8b02802f3abea86e169c6a
leonardo-m-pacheco <92610569+leonardo-m-pacheco@users.noreply.github.com>
2022-03-29 17:20:42 -0300

    .

---
+++ src/ProjetoApi/arquivo.js
@@ -0,0 +1,25 @@
+var myObject = {
+    "customers": [
+        { "CustomerName": "Alfreds Futterkiste", "City": "Berlin", "Country": "Germany" }
+
+    ]
+};
+
+var Objetos = {
+    "Ford": [
+        { "Nome": "Ford", "Modelo": "Escort", "Preco": "R$: 59 900"
+    ,"KM":"160 000" },
+    ]
+};
+var carro2 = {
+    "Wolksvagenfusca": [
+        { "Nome": "Wolksvagen", "Modelo": "Fusca", "Preco": "R$: 115 900"
+    ,"KM":"300 000" },
+    ]
+};
+var carro3 = {
+    "Wolksvagengol": [
+        { "Nome": "Wolksvagen", "Modelo": "Gol", "Preco": "R$: 29 900"
+    ,"KM":"60 000" },
+    ]
+};
---
+++ src/teste/objetos.js
@@ -0,0 +1,7 @@
+
+    
+    w3.displayObject("ford", Objetos);
+    w3.displayObject("wolksvagenfusca", carro2);
+    w3.displayObject("wolksvagengol", carro3);
+ 
+    
